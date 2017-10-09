8d551bc7d5412689a3333521f0425a973f387468
Jose Estudillo <estudillo.jose@gmail.com>
2017-10-09 12:34:12 +0100

    Mon Oct  9 12:34:12 BST 2017

---
+++ js/disqus.js
@@ -0,0 +1,13 @@
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'joseestudillo'; // required: replace example with your forum shortname
+var disqus_identifier = window.location.href; //'a unique identifier for each page where Disqus is present';
+var disqus_title = 'a unique title for each page where Disqus is present';
+//var disqus_url = 'a unique URL for each page where Disqus is present'; // window.location.href used by default
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+    
